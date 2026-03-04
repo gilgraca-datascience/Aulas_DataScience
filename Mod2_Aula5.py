fbd586929c5b38621b359621855254bfa8c3b2a2
Gilberto <gilgraca@gmail.com>
2026-03-04 15:46:33 -0300

    my frist

---
+++ Mod2_Aula5.py
@@ -0,0 +1,9 @@
+# Git (Csistema de ontrole de versão de arquivos) e GitHub Controle de versão parte 1. Ex: tem uma função que não se usa mais, mas o chefe pediu pra usar novamente. Como recuperar essa versão pra usar a função?
+
+# O Git é um software que precisa ser instalado no computador. e depois criar o repositório num diretório para salvamento das versões. Isso se faz abrindo o proprio terminal do Git 
+
+# No controle de versão para poder salvar na núvem, o que é que faz de fato, precisa usar o terminal para tanto:
+
+# usar esse código para salvar uma versão, toda vez que houver uma mudança:
+## git add Mod2_Aula5.py  # Esse serve para adicionar o arquivo
+## gir commit -m "my first"
